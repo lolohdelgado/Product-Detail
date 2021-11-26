48d995deee455eca51ca815fc54db48e3a33bd24
Leila Delgado <lelu7201@gmail.com>
2021-11-26 16:57:39 -0300

    Updates

--- src/components/ProductDetail/ProductImage/ProductImage.js
+++
@@ -1,7 +0,0 @@
-const ProductImage = ({currentWatch}) => (
-    <div className='productImage'>
-        <img src={currentWatch.imageUrl} alt={currentWatch.styleName}/>
-    </div>
-);
-
-export default ProductImage;
